6ac56f7c0254b4e09d1b57d4b426276f966bc450
Daria Milashenko <dashamdo@gmail.com>
2022-08-09 23:34:24 +0300

    stunning

--- js/stunnig-spots.js
+++ js/stunnig-spots.js
@@ -7,22 +7,23 @@
    '<div class="stCarousel"><img src="img/carousal_5.jpg" alt="photo"></div>',
    '<div class="stCarousel"><img src="img/carousal_6.jpg" alt="photo"></div>'
   ];
+
   let actualSlide = 0;
 
   function reviveCarousel() {
     const slidesContainer = document.querySelector(".stunningCarousal .stCarousel");
     slidesContainer.innerHTML = slides[actualSlide];
-    if (window.innerWidth >= 650) {
+    if (window.innerWidth >= 600) {
       const secondSlide = actualSlide + 1  >= slides.length ? 0 : actualSlide + 1;
       slidesContainer.innerHTML += slides[secondSlide];
-      if (window.innerWidth >= 999) {
+      if (window.innerWidth >= 1000) {
         const nextSlide = secondSlide + 1 >= slides.length ? 0 : secondSlide + 1;
         slidesContainer.innerHTML += slides[nextSlide];
       }
     }
   }
 
- function followingSlide() {
+ function followingSlide() { 
   actualSlide = actualSlide + 1 >= slides.length ? 0 : actualSlide + 1;
   reviveCarousel();
  }
@@ -30,11 +31,11 @@
   actualSlide = actualSlide - 1 <= 0 ? slides.length -1 : actualSlide - 1;
   reviveCarousel();
  } 
-
  const btnNext = document.querySelector(".stunningCarousal .next");
  btnNext.addEventListener('click', followingSlide);
  const btnBack = document.querySelector(".stunningCarousal .back");
  btnBack.addEventListener('click', prevSlide);
+
 
  reviveCarousel();
  window.addEventListener("resize", reviveCarousel);
